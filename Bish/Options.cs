a54a2b0a11ad5c8a6ba795b6ae353b49f34071fe
ZZ H <HZZcode@outlook.com>
2026-06-13 22:51:28 +0800

    remove command-line-parser dependency

--- Bish/Options.cs
+++ Bish/Options.cs
@@ -1,25 +1,77 @@
-﻿using CommandLine;
-using JetBrains.Annotations;
+﻿using System.Diagnostics.CodeAnalysis;
+using BishRuntime;
 
 namespace Bish;
 
 internal class Options
 {
-    [Option('c', "command", Required = false, HelpText = "Runs the command string.", SetName = "input")]
-    public string? Command { get; [UsedImplicitly] set; }
-    
-    [Option('f', "file", Required = false, HelpText = "Reads from the file.", SetName = "input")]
-    public string? File { get; [UsedImplicitly] set; }
+    private const string HelpMsg =
+        """
+        * Bish Command Line Help *
+        -c, --command: Runs the command string.
+        -f, --file: Reads from the file.
+        -o, --output: Compile and output to file.
+        -s, --skip: Compile without executing.
+        -i, --interactive: Enter REPL after executing.
+        -l, --lsp: Run LSP only.
+        -h, --help: Show this help message.
+        """;
 
-    [Option('o', "output", Required = false, HelpText = "Compile and output to file.")]
-    public string? Output { get; [UsedImplicitly] set; }
+    public ICodeSource? Source;
+    public string? Output;
+    public bool SkipExecution;
+    public bool Interactive;
+    public bool Server;
+    public bool Help;
 
-    [Option('s', "skip", Required = false, HelpText = "Compile without executing.")]
-    public bool SkipExecution { get; [UsedImplicitly] set; }
+    public static Options Parse(string[] arguments)
+    {
+        var args = new Stack<string>(arguments.Reverse());
+        var options = new Options();
+        while (args.TryPop(out var arg))
+            switch (arg)
+            {
+                case "-c" or "--command":
+                    if (!args.TryPop(out var command)) Error($"{arg} expect a command");
+                    if (options.Source is not null) Error("found multiple input sources");
+                    options.Source = new VirtualSource("<input>", command);
+                    break;
+                case "-f" or "--file":
+                    if (!args.TryPop(out var file)) Error($"{arg} expect a file");
+                    if (options.Source is not null) Error("found multiple input sources");
+                    options.Source = new FileSource(file);
+                    break;
+                case "-o" or "--output":
+                    if (!args.TryPop(out var output)) Error($"{arg} expect an output");
+                    options.Output = output;
+                    break;
+                case "-s" or "--skip": options.SkipExecution = true; break;
+                case "-i" or "--interactive": options.Interactive = true; break;
+                case "-l" or "--lsp": options.Server = true; break;
+                case "-h" or "--help": options.Help = true; break;
+            }
 
-    [Option('i', "interactive", Required = false, HelpText = "Enter REPL after executing.")]
-    public bool Interactive { get; [UsedImplicitly] set; }
+        return options switch
+        {
+            { SkipExecution: true, Output: null } => Error("skip execution is invalid without output file"),
+            { Source: null, Output: not null } => Error("output is invalid without input"),
+            { Help: true } => ShowHelp(),
+            _ => options
+        };
+    }
 
-    [Option('l', "lsp", Required = false, HelpText = "Run LSP only.")]
-    public bool Server { get; [UsedImplicitly] set; }
+    [DoesNotReturn]
+    public static Options Error(string msg)
+    {
+        Console.Error.WriteLine(msg);
+        return ShowHelp();
+    }
+
+    [DoesNotReturn]
+    public static Options ShowHelp()
+    {
+        Console.Error.WriteLine(HelpMsg);
+        Environment.Exit(1);
+        return null!;
+    }
 }
--- Bish/Program.cs
+++ Bish/Program.cs
@@ -1,33 +1,23 @@
 ﻿using BishRuntime;
-using CommandLine;
 
 namespace Bish;
 
 public static class Program
 {
-    public static Task Main(string[] args) =>
-        Parser.Default.ParseArguments<Options>(args).WithParsedAsync(RunOptionsAsync);
-
-    private static async Task RunOptionsAsync(Options options)
+    public static async Task Main(string[] args)
     {
         BishCompiler.BishCompiler.Init();
+        var options = Options.Parse(args);
         switch (options)
         {
             case { Server: true }:
                 await Server.RunAsync();
                 break;
-            case { SkipExecution: true, Output: null }:
-                throw new ArgumentException("-s is invalid without output file");
-            case { Command : not null, File: not null }:
-                throw new ArgumentException("-c and -f cannot work together");
-            case { Command : null, File: null }:
-                if (options.Output is not null) throw new ArgumentException("-o is invalid without input");
+            case { Source: null }:
                 new Repl().Loop();
                 break;
             default:
-                var frame = options.Command is null
-                    ? BishCompileService.Compile(new FileSource(options.File!))
-                    : BishCompileService.Compile(new VirtualSource("<input>", options.Command));
+                var frame = BishCompileService.Compile(options.Source);
                 if (options.Output is { } output)
                 {
                     await using var stream = File.Create(output);

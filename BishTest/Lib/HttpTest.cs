9bf2d1768b22e18043ad99078094556d9e7bde74
ZZ H <HZZcode@outlook.com>
2026-06-19 16:39:25 +0800

    fix github actions

--- BishTest/Lib/HttpTest.cs
+++ BishTest/Lib/HttpTest.cs
@@ -1,11 +1,13 @@
-﻿namespace BishTest.Lib;
+﻿using System.Runtime.InteropServices;
+
+namespace BishTest.Lib;
 
 public class HttpTest : LibTest
 {
-    public static readonly string Url = Environment.GetEnvironmentVariable("CI") == "true"
-        ? "http://localhost:8080"
-        : "https://httpbin.org";
-    
+    public static readonly bool IsCi = Environment.GetEnvironmentVariable("CI") == "true";
+    public static readonly string Url = IsCi ? "http://localhost:8080" : "https://httpbin.org";
+    public static readonly bool Skip = IsCi && !RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
     public HttpTest(TestInfoFixture fixture) : base(fixture, "http", ["Client"])
     {
         Execute("{.fetch}:=Client();");
@@ -16,6 +18,8 @@ public class HttpTest : LibTest
     [Fact]
     public void TestFetch()
     {
+        if (Skip) return;
+
         Execute($"r:=await fetch('{Url}/status/200');");
         ExpectResult("r.status", "200");
         ExpectTrue("r.success");
@@ -27,7 +31,7 @@ public class HttpTest : LibTest
         Execute($"r:=await fetch('{Url}/get?name=zz_404&age=18');");
         ExpectTrue("r.success");
         ExpectResult("parse(await r.content)['args']", "data");
-        
+
         Execute("o:={.headers:{'X-Custom':'ZZ_404'}};");
         Execute($"r:=await fetch('{Url}/headers',o);");
         ExpectTrue("r.success");
@@ -37,6 +41,8 @@ public class HttpTest : LibTest
     [Fact]
     public void TestPost()
     {
+        if (Skip) return;
+
         Execute("o:={.method:'POST',.body:stringify(data),.mediaType:'application/json'};");
         Execute($"r:=await fetch('{Url}/post',o);");
         ExpectTrue("r.success");

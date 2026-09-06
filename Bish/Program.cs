e5fedb22aa5f1b69504dd49a4fe0aeee867e8e1a
ZZ H <HZZcode@outlook.com>
2026-09-06 22:47:07 +0800

    make lsp async

--- Bish/Program.cs
+++ Bish/Program.cs
@@ -1,24 +1,15 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using BishRuntime;
+﻿using BishRuntime;
 
 namespace Bish;
-
-public static class Server
-{
-    [Builtin]
-    [SuppressMessage("Usage", "VSTHRD002")]
-    public static void Run() => LSP.Server.RunAsync().GetAwaiter().GetResult();
-
-    public static readonly BishType StaticType = new("Server");
-}
 
 public static class Program
 {
     public static void Main(string[] args)
     {
         BishCompiler.BishCompiler.Init();
-        BuiltinsRegistry.Register();
+        var main = BishImporter.Import(null, "$self/main").GetMember("main");
+        var lsp = new BishNativeTask(LSP.Server.RunAsync);
         var argv = args.Select(arg => new BishString(arg)).ToArray();
-        BishImporter.Import(null, "$self/main").GetMember("main").Call(new BishArgs([Server.StaticType, ..argv]));
+        BishTaskRunner.Block(main.Call(new BishArgs([lsp, ..argv])));
     }
 }
